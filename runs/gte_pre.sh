a08096e54f14a88b434a29d970bb936d8c39625d
gzy <1154459434@qq.com>
2025-08-24 09:11:46 +0000

    modified:   commands.list 	modified:   config/default_config.yml 	modified:   runs/gte_pre.sh

--- runs/gte_pre.sh
+++ runs/gte_pre.sh
@@ -4,16 +4,16 @@ python batch_pretrain_simple.py \
     --datasets zinc,molhiv,peptides_func --methods feuler,fcpp,topo --bpe_scenarios all,raw,random \
     --epochs 100 --experiment_group gte_exploration \
     --use_augmentation false \
-    --commands_stdout\
+    --commands_only\
     --plain_logs  
 
 # 生成5种微调命令：BERT + GTE的4种训练方式
 python batch_finetune_simple.py \
-    --finetune_modes bert,gte-direct,gte-pretrain,gte-reset-direct,gte-reset-pretrain \
+    --finetune_modes bert,gte-pretrain,gte-reset-pretrain \
     --datasets zinc,molhiv,peptides_func --methods feuler,fcpp,topo --bpe_scenarios all,raw,random \
     --epochs 60 --experiment_group gte_exploration \
     --use_augmentation false \
-    --commands_stdout \
+    --commands_only \
     --plain_logs 
  
 #  # 生成3种预训练命令：BERT + GTE保持权重 + GTE清空权重
