7f0ecdfae6d979ec72d1fc5d0d0a43a1734cbc1f
gzy <1154459434@qq.com>
2025-08-24 09:29:17 +0000

    modified:   runs/gte_dir.sh 	modified:   runs/gte_pre.sh

--- runs/gte_dir.sh
+++ runs/gte_dir.sh
@@ -3,7 +3,7 @@ python batch_finetune_simple.py \
     --datasets zinc,molhiv,peptides_func --methods feuler,fcpp,topo --bpe_scenarios all,raw,random \
     --epochs 60 --experiment_group gte_exploration \
     --use_augmentation false \
-    --commands_only \
+    --commands_stdout \
     --plain_logs 
  
 #  # 生成3种预训练命令：BERT + GTE保持权重 + GTE清空权重
