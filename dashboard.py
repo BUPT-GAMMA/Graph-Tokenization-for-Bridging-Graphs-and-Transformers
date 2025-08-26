ef96ce0e501e1b2e0e95a3c53c6134ed342d7b69
gzy <1154459434@qq.com>
2025-08-27 01:59:26 +0800

    Refine hyperparameter tuning in zinc_hyperopt.py and add Optuna dashboard support
    - Adjusted learning rate range for better optimization.
    - Updated batch size options to limit to larger sizes.
    - Modified weight decay, gradient norm, mask probability, and warmup ratio ranges for improved training dynamics.
    - Introduced a new dashboard.py file to run the Optuna dashboard for monitoring hyperparameter optimization.

---
+++ dashboard.py
@@ -0,0 +1,13 @@
+import optuna
+from optuna_dashboard import run_server
+from optuna.storages import JournalStorage, JournalFileStorage
+
+
+
+
+if __name__ == "__main__":
+    storage = JournalStorage(JournalFileStorage('./journal/zinc_hyperopt.db'))
+
+
+    # Start the Optuna Dashboard server on localhost:8080
+    run_server(storage, host="localhost", port=18080)
