bc8aa61e669b0f7a2460f3dce07fac0e1d4d6090
gzy <1154459434@qq.com>
2025-08-28 19:19:33 +0800

    更新默认配置，禁用多重采样功能以简化设置；更新预训练脚本，调整训练周期至100以提升模型性能；新增资源检查脚本，优化节点扫描逻辑，确保准确列出可用GPU资源。

--- runs/gte_pre.sh
+++ runs/gte_pre.sh
@@ -2,7 +2,7 @@ python batch_pretrain_simple.py \
     --encoder bert,gte \
     --datasets zinc --bpe_scenarios all,raw,random \
     --methods eulerian,feuler,cpp,fcpp,smiles,topo \
-    --epochs 60 --experiment_group pre_formula \
+    --epochs 100 --experiment_group pre_formula \
     --commands_stdout
  
 
